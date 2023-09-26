196aefbe420932ba6dc3dd89dfc58688cc1cdc35
ailish432 <85789349+ailish432@users.noreply.github.com>
2023-09-26 21:42:56 +0100

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    modulePathIgnorePatterns: ['dist'],
-    testMatch: ['**/*.test.ts'],
-    "preset": "ts-jest",
-    moduleNameMapper: {
-      '^@tests/(.*)': '<rootDir>/tests/$1',
-      '^@source/(.*)': '<rootDir>/$1',
-    }
-  };
