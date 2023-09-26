8a9bec7cc5791af1acf29ec44ba616040398316d
ailish432 <85789349+ailish432@users.noreply.github.com>
2023-09-26 21:42:35 +0100

    Delete dist directory

--- dist/FormatPacket/convertBufferToArray.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertBufferToArray = void 0;
-const convertBufferToArray = (packets) => {
-    const packetString = packets.toString();
-    return packetString.split('\n');
-};
-exports.convertBufferToArray = convertBufferToArray;
-//# sourceMappingURL=convertBufferToArray.js.map
